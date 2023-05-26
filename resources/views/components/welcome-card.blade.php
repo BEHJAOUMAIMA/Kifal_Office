a21a566ef31852b0743f6d31b3b0f1fd1b812e6f
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-05-26 13:42:47 +0100

    add component

---
+++ resources/views/components/welcome-card.blade.php
@@ -0,0 +1,28 @@
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col-xl-12 mb-4 col-lg-12 col-12">
+            <div class="card">
+                <div class="d-flex align-items-center row justify-content-between">
+                    <div class="col-sm-12 col-md-7 col-xl-7 col-lg-7">
+                        <div class="card-body  ms-sm-0 ms-md-5 ms-lg-5 ms-xl-5">
+                            <h4 class="card-title mb-4 text-primary">Bienvenue  {{ auth()->user()->getUserFullName() }} 🎉</h4>
+                            <h6 class="mb-3" style="font-size: 15px">Ravis de vous accueillir dans notre tableau de bord dédié à la vente de voitures. </h>
+                            <h6 class="mb-2" style="font-size: 15px">Préparez-vous à vivre une expérience de conduite exceptionnelle au cœur de notre entreprise !</h6>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-xl-4 col-lg-4 text-center text-sm-left">
+                        <div class="card-body pb-0 px-0 px-md-4">
+                        <img
+
+                            src="../../assets/img/illustrations/card-advance-sale.png"
+                            height="140"
+                            alt="view sales" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
