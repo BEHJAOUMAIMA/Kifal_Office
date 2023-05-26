a3018d99b13c0405c07ecb66b436be93a3e643fe
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-05-26 16:08:48 +0100

    update PRofile view

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -2,7 +2,7 @@
 <li class="nav-item navbar-dropdown dropdown-user dropdown">
     <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
         <div class="avatar avatar-online">
-            <img src="../../assets/img/avatars/1.png" alt class="h-auto rounded-circle" />
+            <img src="../../assets/img/avatars/utilisateur.png" alt class="h-auto rounded-circle" />
         </div>
     </a>
     {{-- @dd($user) --}}
@@ -13,7 +13,7 @@
                     <div class="flex-shrink-0 me-3">
                         <div class="avatar avatar-online">
                             <a href="{{url('/myProfile')}}">
-                                <img src="../../assets/img/avatars/1.png" alt class="h-auto rounded-circle"/>
+                                <img src="../../assets/img/avatars/utilisateur.png" alt class="h-auto rounded-circle"/>
                             </a>
                         </div>
                     </div>
