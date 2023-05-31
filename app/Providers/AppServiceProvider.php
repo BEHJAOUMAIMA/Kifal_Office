465b5924a6863d88a20b7c15aaade07c10bce8b4
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-05-31 18:41:28 +0100

    add customed blade directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,7 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
-        //
+
+        Blade::if('role', function ($role) {
+            return auth()->check() && auth()->user()->role->role_name === $role;
+        });
+
+        Blade::if('permission', function ($permission) {
+            return auth()->check() && auth()->user()->role->permissions->contains('permission_name', $permission);
+        });
     }
 }
