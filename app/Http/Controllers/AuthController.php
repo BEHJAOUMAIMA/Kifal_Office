ba1ae4a8e9016923260843b02b4c75ac140bea89
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-06-02 11:25:43 +0100

    update log out function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,19 +19,6 @@ class AuthController extends Controller
 
     public function login(StoreUserRequest $request)
     {
-
-        // $credentials = $request->only('email', 'password');
-        // $user = User::where('email', $request->email)->first();
-        // if (!$user || !($request->password == $user->password)) {
-        //     return back()->withErrors([
-        //         'email' => 'Les informations d\'identification fournies ne sont pas valides.',
-        //     ]);
-        // } else {
-        //     // $userAuth = Auth::user();
-        //     $token = $user->createToken('auth_token')->plainTextToken;
-        //     // $userAuth->setRememberToken($token);
-        //     return redirect('/Acceuil')->with(['token' => $token]);
-        // }
         try {
             $credentials = [
                 'email' => $request->get('email'),
@@ -51,27 +38,11 @@ class AuthController extends Controller
             Log::error($e->getMessage());
         }
 
-
-        // return back()->withErrors([
-        //     'email' => 'Les informations d\'identification fournies ne sont pas valides.',
-        // ]);
     }
 
     public function logout(Request $request)
     {
-        // $user = Auth::user();
-        // dd($user);
-        // if ($user) {
-        //     $user->currentAccessToken()->delete();
-        // }
-
-        // // Invalidating session cookies
-        // Http::withHeaders([
-        //     'X-XSRF-TOKEN' => csrf_token(),
-        // ])->post(config('sanctum.stateful') ? '/sanctum/csrf-cookie' : '/csrf-cookie');
-
-        // return redirect('/')->with('success', 'Déconnexion réussie');
-        Auth::logout();
+        Auth::user()->tokens()->delete();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('/')->with('success', 'Vous avez été déconnecté avec succès. Revenez bientôt !');
