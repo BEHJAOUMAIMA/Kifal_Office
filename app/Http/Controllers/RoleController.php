912e0bbeaab5abd46587b1b856a71fd50e6b9986
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-06-08 10:30:23 +0100

    add index & store function for Role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,65 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RoleController extends Controller
 {
     public function index(Role $role)
     {
-        $role = Role::find($role->id);
-        return view('pages.role', ['role' => $role]);
+        $roles = Role::all();
+        $permissions = Permission::all();
+        $permissions_categories = collect($permissions)->pluck('permission_category')->unique()->values()->prepend('Modules')->toArray();
+        $permissions = collect($permissions)->groupBy('permission_module')->map(function ($val, $key) {
+            return [
+                "module" => $key,
+                "data" => $val->toArray()
+            ];
+
+        })->values()->toArray();
+        $permissions_role = $role->permissions();
+        return view('pages.role', [
+            'roles' => $roles,
+            "permissions" => $permissions,
+            "permissions_role" => $permissions_role,
+            "permissions_category" => $permissions_categories,
+            'role' => $role,
+        ]);
     }
 
     public function create()
     {
-        return view('roles.create');
+
     }
 
-    public function store(Request $request)
+    public function store(Request $request, Role $role)
     {
-        // Validate Data form
-        $validatedData = $request->validate([
+         $validatedData = $request->validate([
             'role_name' => 'required|unique:roles',
-        ],[
-            'role_name.required'=>'Veuiller Entrer un Role Valide !!',
+            'role_category' => 'required',
+            'permissions' => 'array'
+        ], [
+            'role_name.required' => 'Veuillez entrer un rôle valide !',
+            'role_category.required' => 'Veuillez sélectionner une catégorie pour le rôle !'
         ]);
 
-        // Create new Role
-        Role::create($validatedData);
+        $role = Role::create([
+            'role_name' => $validatedData['role_name'],
+            'role_category' => $validatedData['role_category']
+        ]);
 
-        // Redirect with success msg
-        return redirect()->route('roles.index')->with('success', 'Le rôle a été créé avec succès.');
+        $permissions = $request->input('permissions', []);
+        foreach ($permissions as $permissionId) {
+            DB::table('permission_role')->insert([
+                'role_id' => $role->id,
+                'permission_id' => $permissionId
+            ]);
+        }
+
+        return redirect()->back()->with('success', 'Le rôle a été ajouté avec succès !');
+
     }
 
     public function edit(Role $role)
     {
-        return view('roles.edit', compact('role'));
+
     }
 
     public function update(Request $request, Role $role)
     {
-         // Validate Data form
-         $validatedData = $request->validate([
-            'role_name' => 'required|unique:roles',
-        ],[
-            'role_name.required'=>'Veuiller Entrer un Role Valide !!',
-        ]);
-
-        // Update Role
-        $role->update($validatedData);
-
-        // Redirect with success msg
-        return redirect()->route('roles.index')->with('success', 'Le rôle a été mis à jour avec succès.');
+        
     }
 
     public function destroy(Role $role)
     {
-        // delete Role
-        $role->delete();
-
-        // Redirect with success msg
-        return redirect()->route('roles.index')->with('success', 'Le rôle a été supprimé avec succès.');
+        
     }
 }
