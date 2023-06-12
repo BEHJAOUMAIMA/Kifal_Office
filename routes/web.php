f612ec40b7296c6238c4129d8e18d9f8a1906f76
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-06-12 11:04:50 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
@@ -23,18 +25,12 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
 
 
-Route::get('/Utilisateurs', function () {
-    return view('pages/users/allUsers');
-});
+
 Route::get('/Utilisateurs/informations', function () {
     return view('pages/users/showUser');
 });
-Route::get('/myProfile', function () {
-    return view('pages/userProfile');
-});
-Route::get('/myAccount', function () {
-    return view('pages/userAccount');
-});
+
+
 Route::get('/EditUser', function () {
     return view('pages/users/editUser');
 });
@@ -56,15 +52,27 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/Acceuil', [DashboardController::class, 'getDashboard'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/role/permissions/{role}', [PermissionController::class, 'index'])
-    ->where('role', '[0-9]+')
-    ->name('permissions');
-
+            ->where('role', '[0-9]+')
+            ->name('permissions');
     Route::post('/roles/{role}/update-permissions', [PermissionController::class, 'updateRolePermissions'])
-    ->name('roles.updatePermissions');
-
+             ->name('roles.updatePermissions');
     Route::get('/roles', [RoleController::class, 'index'])
-    ->where('role', '[0-9]+')
-    ->name('roles');
-
+            ->where('role', '[0-9]+')
+            ->name('roles');
     Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('/myProfile/{user}', [ProfileController::class, 'displayProfile'])->name('myProfile');
+    Route::get('/myAccount/{user}',  [ProfileController::class, 'displayAccount'])->name('myAccount');
+    Route::post('/myAccount/edit/{user}',  [ProfileController::class, 'editProfile'])->name('myAccount.edit');
+    Route::post('/change-password', [ProfileController::class, 'changePassword'])->name('change.password');
+    Route::post('/delete-account', [ProfileController::class, 'deleteAccount'])->name('delete.account');
+
+
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function (){
+    Route::get('/utilisateurs', [UserController::class, 'index'])->name('users.display');
+    Route::post('/add/utilisateur', [UserController::class, 'addNewUser'])->name('user.add');
+    Route::get('/utilisateurs/informations/{user}', [UserController::class, 'showUser'])->name('user.show');
+    Route::post('/utilisateur/{user}', [UserController::class, 'updateUser'])->name('user.update');
+
 });
